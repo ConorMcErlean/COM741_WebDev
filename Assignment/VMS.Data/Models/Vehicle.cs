4d09b83141dad5e47fa9712e57e29f6c23630105
Conor Mc <Conor@cmcerlean.co.uk>
2020-04-14 14:57:15 +0100

    Added Service & Vehicle Models. Added VehicleDBContext

--- Assignment/VMS.Data/Models/Vehicle.cs
+++ Assignment/VMS.Data/Models/Vehicle.cs
@@ -7,6 +7,31 @@ namespace VMS.Data.Models
 {      
     public class Vehicle
     {
-       
-    }
+        // Primary Key
+        public int Id {get; set;}
+        [Required]
+        public string Make {get; set;}
+        [Required]
+        public string Model {get; set;}
+        public DateTime RegDate {get; set;}
+        public string RegNumber {get; set;}
+        // Age will set a private variable based on RegDate
+        private int _age;
+        public int age 
+        {   
+            get { return _age; }
+            set {
+                _age = (int) ((DateTime.Now - RegDate).TotalDays)/365;
+            }
+        }// age
+        public string TransmissionType {get; set;}
+        [Range(0, 300)]
+        public string CO2Rating {get; set;}
+        public string FuelType {get; set;}
+        public string BodyType {get; set;}
+        public string Doors {get; set;}
+        public UrlAttribute photo {get; set;}
+
+        IList<Service> Services { get; set;}
+    }// Vehicle Class
 }
--- Assignment/VMS.Data/Repositories/VehicleDbContext.cs
+++ Assignment/VMS.Data/Repositories/VehicleDbContext.cs
@@ -8,7 +8,30 @@ namespace VMS.Data.Repositories
 {
     public class VehicleDbContext : DbContext
     {
-        // complete the context
-        
+        // Database context using SQLite
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Filename=data.db")
+            /* Optional Logging (Comment in/out based on need) */
+            // .UseLoggerFactory( new ServiceCollection ()
+            //                     .AddLogging(builder => builder.AddConsole())
+            //                     .BuildServiceProvider
+            //                     .GetService<ILoggerFactory>())
+            /* End of Optional Logging */
+            ;
+        }
+
+        // Allow Querying of Vehicles Table
+        public DbSet<Vehicle> Vehicles { get; set;}
+
+        // Allow Querying of Services Table
+        public DbSet<Service> Services { get; set;}
+
+        // Initialising Tables
+        public void Initialise()
+        {
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
     }
 }
