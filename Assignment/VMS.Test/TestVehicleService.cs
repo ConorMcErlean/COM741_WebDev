5da51f8f41af3a2c6041b9a27858039ff724d59f
Conor Mc <Conor@cmcerlean.co.uk>
2020-04-14 15:59:36 +0100

    Beginning Creation of Tests

--- Assignment/VMS.Test/TestVehicleService.cs
+++ Assignment/VMS.Test/TestVehicleService.cs
@@ -7,9 +7,32 @@ using VMS.Data.Services;
 
 namespace VMS.Test
 {
+    
     public class TestVehicleService
     {
+        // Service
+        private readonly IVehicleService svc;
+        private TestVehicleService()
+        {
+            // General Arangement
+            svc = new VehicleDbService;
+            // Ensure clean slate Database
+            svc.Initialise();
+        }
+        
        // define a set of appropriate tests to test the vehicle service class
+       [Fact]
+       public void AddVehicle_WhenNone_ShouldNotReturnNull()
+       {
+       //Given
+       var car = new Vehicle();
+       
+       //When
+       var v = svc.AddVehicle(car);
+       
+       //Then
+       Assert.NotNull(v);
+       }
  
     }
 }
