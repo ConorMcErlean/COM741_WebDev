f29adc442340bfd080b6288f77ef1b3d39e60104
Conor Mc <Conor@cmcerlean.co.uk>
2020-04-17 13:23:37 +0100

    Vehicles Index Page added, alongside back-end vehicle controller

--- Assignment/VMS.Web/Controllers/VehicleController.cs
+++ Assignment/VMS.Web/Controllers/VehicleController.cs
@@ -12,6 +12,32 @@ namespace VMS.Web.Controllers
 
  public class VehicleController : Controller
     {
-        
-    }
+        // Service to carry out buisness logic
+        private readonly IVehicleService svc;
+        public VehicleController()
+        {
+            svc = new VehicleDbService();
+        }// Vehicle Controller
+
+        // Get /Vehicle/Index
+        public IActionResult Index()
+        {
+            // Retrieve list of vehicles
+            var VehicleList = svc.GetAllVehicles();
+
+            //Render a view with the list
+            return View(VehicleList);
+        }
+
+        // Get /Vehicle/Details/{id}
+        public IActionResult Details(int id)
+        {
+            // Retrieve required vehicle
+            var Vehicle = svc.GetVehicleById(id);
+
+            // Render view with vehicle
+            return View(Vehicle);
+        }
+    }// Vehicle Controller
+
 }
