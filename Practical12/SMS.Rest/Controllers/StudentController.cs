dca22128339d12d1705c93e70d0db8d04f16d1d7
Conor Mc <Conor@cmcerlean.co.uk>
2020-04-24 14:27:24 +0100

    Completed StudentController

--- Practical12/SMS.Rest/Controllers/StudentController.cs
+++ Practical12/SMS.Rest/Controllers/StudentController.cs
@@ -10,7 +10,7 @@ using SMS.Rest.Dtos;
 namespace SMS.Rest.Controllers
 {
 
-    [ApiController]
+    [ApiController]ic
     [Route("api/[controller]")]
     public class StudentController : ControllerBase
     {
@@ -21,6 +21,50 @@ namespace SMS.Rest.Controllers
         }
 
         // add actions to support CRUD methods listed in practical questions
-        
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(_service.GetAllStudents());
+        }
+        [HttpGet]
+        public IActionResult Get(int id)
+        {
+            return Ok(_service.GetStudent(id));
+        }
+
+        [HttpPost]
+        public IActionResult Create(StudentDto s)
+        {
+
+            if (ModelState.IsValid)
+            {
+                var Student = _service.AddStudent(s.Name, s.Email, s.Course, s.Age, s.Grade);
+                if (Student != null)
+                {
+                    CreatedAtAction(nameof(Get), new {Id = Student.Id}, Student);
+                }
+            }
+            return BadRequest(ModelState);
+        }//Create
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var ok = _service.DeleteStudent(id);
+            if (ok)
+            {
+                return Ok();
+            }
+            return NotFound();
+        }//Delete
+        [HttpPut]
+        public IActionResult Update(int id, StudentDto s)
+        {
+            var result = _service.UpdateStudent(id, s);
+            if (result != null)
+            {
+                return Ok(_service.GetStudent(id));
+            }
+            return NotFound();
+        }//Update
     }
 }
